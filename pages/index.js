7f26e79fbec1639f6c5866c2ee7ec02973121026
alicoder5 <mukhammadali.ubaydullayev.05@inbox.ru>
2022-11-07 22:14:18 +0500

    test

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import {useRouter} from "next/router";
+import {useEffect} from "react";
+
+const Index = () => {
+  const router = useRouter()
+  // useEffect(()=> {
+  //   router.push('/main')
+  //   // eslint-disable-next-line
+  // }, [])
+}
+
+export default Index
---
+++ pages/main/index.jsx
@@ -0,0 +1,16 @@
+// import Root from "../../root";
+import {Container} from "./style";
+// import MapProducts from "../../components/MainComponents/MapProducts";
+
+const Main = () => {
+    return(
+        // <Root>
+            <Container>
+                {/*<MapProducts />*/}
+                Main
+            </Container>
+        // </Root>
+    )
+}
+
+export default Main
